5a27b6a36127c1ca41893456a255e41e089046bb
eric.chang <eric.chang@promeritage.com.tw>
2020-03-19 11:28:04 +0800

    addVixTest

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const fs = require('fs');
 const request = require('request');
 const cookieParser = require('cookie-parser')
 
+const rp = require('request-promise')
+const cheerio = require("cheerio");
+
+
+
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -73,7 +78,28 @@ function pushMessage(message, token, callback) {
             'Content-Type': 'application/x-www-form-urlencoded',
         },
         auth: {
-            'bearer': token
+            'bearer': 'kAz9ZVRimgANgVCC32s5k06XygFjYkpe0Z5ePZKeolO'
+        },
+        form: {
+            message: message
+        },
+    }
+    request(clientServerOptions, function (error, response, body) {
+        console.log(error, response, body);
+        callback(body)
+        return;
+    });
+}
+
+function pushMessage1(message, callback) {
+    const clientServerOptions = {
+        method: 'POST',
+        uri: 'https://notify-api.line.me/api/notify',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        auth: {
+            'bearer': 'kAz9ZVRimgANgVCC32s5k06XygFjYkpe0Z5ePZKeolO'
         },
         form: {
             message: message
@@ -87,6 +113,81 @@ function pushMessage(message, token, callback) {
 }
 
 
+function getVixDataWithWebPer30Sec() {
+    console.log('getVixData');
+    // 每30爬一次價格後回傳
+
+    rp(
+        {
+            url: "https://websys.fsit.com.tw/FubonETF/Trade/RealTimeNav.aspx?lan=TW&area=FU",
+            // url: "http://localhost:3005/testVixHtm",
+            method: "GET"
+        }
+    ).then((aa) => {
+
+        console.log(" new 有來打大樂透")
+
+        const $ = cheerio.load(aa); // 載入 body
+        console.log("$:", $);
+        // const result = []; // 建立一個儲存結果的容器
+        // https://andy6804tw.github.io/2018/02/11/nodejs-crawler/
+        const today = new Date()
+
+        const no1 = $('table tr').text();
+        console.log('no1:', no1);
+        str = no1.replace(/\s*/g, "");
+        console.log(str);
+        // console.log('及時淨值:' ,str.split("即時估計淨值B")[1].substr(18,5));
+        // console.log('及時淨值時間:',str.split("即時估計淨值B")[1].substr(0,18));
+        // console.log('即時成交價格:' ,str.split("即時成交價格D")[1].substr(18,5));
+        // console.log('即時成交時間:',str.split("即時成交價格D")[1].substr(0,18));
+        // console.log('即時折溢價幅:',str.split("即時折溢價幅(D-B)/B")[1].substr(18,6));
+
+
+        a = '及時淨值:' + str.split("即時估計淨值B")[1].substr(18, 5);
+        b = '及時淨值時間:' + str.split("即時估計淨值B")[1].substr(0, 18);
+        c = '即時成交價格:' + str.split("即時成交價格D")[1].substr(18, 5);
+        d = '即時成交時間:' + str.split("即時成交價格D")[1].substr(0, 18);
+        e = '即時折溢價幅:' + str.split("即時折溢價幅(D-B)/B")[1].substr(18, 6);
+
+
+        console.log(a);
+        console.log(b);
+        console.log(c);
+        console.log(d);
+        console.log(e);
+
+        console.log(a + "\r\n" + b + "\r\n" + c + "\r\n" + d + "\r\n" + e);
+
+        const message = "\r\n"+  a + "\r\n" + b + "\r\n" + c + "\r\n" + d + "\r\n" + e;
+        pushMessage1(message,()=>{ return });
+    });
+
+
+
+
+}
+
+
+app.get('/testVixHtm', (req, res) => {
+    render('vixTest.html', {
+
+    }, function (err, data) {
+        res.send(data);
+    });
+
+
+})
+
+
+app.get('/test', function (req, res) {
+    render('test.html', {
+
+    }, function (err, data) {
+        res.send(data);
+    });
+
+})
 app.get('/register/:clientId', function (req, res) {
     render('auth.html', {
         clientId: req.params.clientId
@@ -129,6 +230,14 @@ app.get('/', function (req, res) {
     });
 });
 
-app.listen(3000, function () {
+
+
+app.listen(3005, () => {
+
     console.log('Example app listening on port 3000!');
+    setInterval(() => {
+        getVixDataWithWebPer30Sec();
+        
+    }, 1000*60*3);
+
 });
